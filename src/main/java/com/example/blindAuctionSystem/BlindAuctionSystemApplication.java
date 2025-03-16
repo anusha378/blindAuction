414f36d9d88de8a7f954e1f70ef4c3acfae723fa
anusha <anusha.channesh21@gmail.com>
2025-03-16 12:16:49 +0000

    Initial Commit

---
+++ src/main/java/com/example/blindAuctionSystem/BlindAuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.blindAuctionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindAuctionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindAuctionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blindAuctionSystem/BlindAuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blindAuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindAuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
